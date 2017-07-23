ee57745097ae055693e3384a12e45affb669d9ec
Onuvob Tripura <onuvobtripura@gmail.com>
2017-07-24 02:40:04 +0600

    Update Problem 2.cpp

--- Problem 2.cpp
+++ Problem 2.cpp
@@ -2,19 +2,18 @@
 using namespace std;
 int main()
 {
-	int cas, num, count1=0, count2=0;
+	int cas, num, count1 = 0, count2 = 0;
 	cin>>cas;
+	
 	while(cas--)
 	{
 		cin>>num;
-		if(num>=0)
-		{
+		
+		if(num >= 0)
 			count1++;
-		}
+			
 		else
-		{
 			count2++;
-		}
 	}
 	cout<<count1<<" "<<count2<<endl;
 	return 0;
